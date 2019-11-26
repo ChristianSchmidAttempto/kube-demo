57b53ed5e5b042076f96723a5e4f29fbd2206c71
ChrisSchmid <christian.schmid@attempto.eu>
2019-11-26 16:11:23 +0100

    update

--- src/main/java/eu/attempto/kube/demo/controller/VersionInfoController.java
+++ src/main/java/eu/attempto/kube/demo/controller/VersionInfoController.java
@@ -1,5 +1,6 @@
 package eu.attempto.kube.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersionInfoController {
     private static final String template = "Hello, %s! this is version 1.0.0";
+
+    @GetMapping
+    public String hey() {
+        return "Hallo, Welt!";
+    }
 
     @RequestMapping("/version")
     public String version(
